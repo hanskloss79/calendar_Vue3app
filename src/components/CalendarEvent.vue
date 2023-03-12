0d53f597c515aac29e3c2c71c3830111c5005227
hanskloss79 <piohandz79@gmail.com>
2023-03-12 21:49:17 +0100

    Events edition, updating and removal functions added to Calendar application. CalendarEvent.vue and store.js files modified.

--- src/components/CalendarEvent.vue
+++ src/components/CalendarEvent.vue
@@ -1,33 +1,63 @@
 <template>
     <div class="day-event" :style="getEventBackgroundColor">
-                <div>
-                    <span class="has-text-centered details">{{ event.details}}</span>
-                    <div class="has-text-centered icons">
-                        <i class="fa fa-pencil-square edit-icon"></i>
-                        <i class="fa fa-trash-o delete-icon"></i>
-                    </div>
-                </div>
+        <div v-if="!event.edit">
+            <span class="has-text-centered details">{{ event.details }}</span>
+            <div class="has-text-centered icons">
+                <i class="fa fa-pencil-square edit-icon" 
+                @click="editEvent(day.id, event.details)"></i>
+                <i class="fa fa-trash-o delete-icon"
+                @click="deleteEvent(day.id, event.details)"></i>
             </div>
+        </div>
+        <!-- the version of event component for event edition-->
+        <div v-if="event.edit">
+            <input type="text" :placeholder="event.details" v-model="newEventDetails" />
+            <div class="has-text-centered icons">
+                <i class="fa fa-check" @click="updateEvent(day.id, event.details, newEventDetails)"></i>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
+import {store} from "../store.js";
+
 export default {
     name: 'CalendarEvent',
 
+    data() {
+        return {
+            newEventDetails: "",
+        };
+    },
+    
     props: ['event', 'day'],
 
     computed: {
         getEventBackgroundColor() {
-            const colors = ['#ff9999', '#85d6ff', '#99ff99' ];
+            const colors = ['#ff9999', '#85d6ff', '#99ff99'];
             let randomColor = colors[Math.floor(Math.random() * colors.length)];
             return `background-color: ${randomColor}`;
         }
-    }
-}
+    },
+
+    methods: {
+        editEvent(dayId, eventDetails){
+            store.editEvent(dayId, eventDetails);
+        },
+        updateEvent(dayId, originalEventDetails, updatedEventDetails) {
+            if (updatedEventDetails === '') updatedEventDetails = originalEventDetails;
+            store.updateEvent(dayId, originalEventDetails, updatedEventDetails);
+            this.newEventDetails = '';
+        },
+        deleteEvent(dayId, eventDetails){
+            store.deleteEvent(dayId, eventDetails);
+        },
+    },
+};
 </script>
 
 <style lang="scss" scoped>
-
 .day-event {
     margin-top: 6px;
     margin-bottom: 6px;
@@ -54,5 +84,4 @@ export default {
         }
     }
 }
-
 </style>
--- src/store.js
+++ src/store.js
@@ -15,6 +15,43 @@ export const store = {
     },
     submitEvent(eventDetails) {
         const activeDay = this.getActiveDay();
-        activeDay.events.push({"details": eventDetails, "edit": false});
+        activeDay.events.push({ "details": eventDetails, "edit": false });
+    },
+    // sets all events to the non-edit state prior to toggling the targeted event
+    resetEditOfAllEvents() {
+        this.state.data.map((dayObj) => {
+            dayObj.events.map((event) => {
+                event.edit = false;
+            });
+        });
+    },
+
+    getEventObj(dayId, eventDetails) {
+        const dayObj = this.state.data.find((day) => day.id === dayId);
+        return dayObj.events.find((event) => event.details === eventDetails);
+    },
+
+    // edition of event in calendar
+    editEvent(dayId, eventDetails) {
+        this.resetEditOfAllEvents();
+        const eventObj = this.getEventObj(dayId, eventDetails);
+        eventObj.edit = true;
+    },
+
+    // updating data of calendar event
+    updateEvent(dayId, originalEventDetails, newEventDetails) {
+        const eventObj = this.getEventObj(dayId, originalEventDetails);
+        eventObj.details = newEventDetails;
+        eventObj.edit = false;
+    },
+
+    // removing event from calendar
+    deleteEvent(dayId, eventDetails){
+        const dayObj = this.state.data.find((day) => day.id === dayId);
+        const eventIndexToRemove = dayObj.events.findIndex(
+            event => event.details === eventDetails
+        );
+        dayObj.events.splice(eventIndexToRemove, 1); 
     }
+
 }
